44fce8f2d93071c06d72370d1a29f147d32c0d10
Prinon <prinonarnob@gmail.com>
2022-05-05 17:05:10 +0600

    TabBar

--- tab_bar/lib/main.dart
+++ tab_bar/lib/main.dart
@@ -17,27 +17,30 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
         length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("PhoneBook"),
-            actions: [
-              IconButton(onPressed: (){}, icon: Icon(Icons.more_vert))
-            ],
-            bottom: const TabBar(tabs: [
-              Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                  child: Text('Recent',
-                      style: TextStyle(fontSize: 12))),
-              Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                  child: Text('Contact',
-                      style: TextStyle(fontSize: 12))),
-            ],),
-          ),
-          body: TabBarView(
-            children: [
-              recent(),contact()
-            ],
+        child: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              toolbarHeight: 8,
+              title: const Text("PhoneBook"),
+              actions: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.more_vert))
+              ],
+              bottom: const TabBar(tabs: [
+                Padding(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                    child: Text('Recent',
+                        style: TextStyle(fontSize: 12))),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                    child: Text('Contact',
+                        style: TextStyle(fontSize: 12))),
+              ],),
+            ),
+            body: TabBarView(
+              children: [
+                recent(),contact()
+              ],
+            ),
           ),
         ),
       ),
