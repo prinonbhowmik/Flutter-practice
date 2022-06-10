f87af0484009743c21bbab3e17f41161577ce3af
Prinon <prinonarnob@gmail.com>
2022-06-11 03:22:24 +0600

    data table

--- data_table_example/lib/main.dart
+++ data_table_example/lib/main.dart
@@ -29,7 +29,30 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
 
       body: Center(
-
+        child: DataTable(
+          columns: const [
+            DataColumn(label:Text("Name")),
+            DataColumn(label:Text("Age")),
+            DataColumn(label:Text("Designation")),
+          ],
+          rows: const [
+            DataRow(cells: [
+              DataCell(Text("Prinon")),
+              DataCell(Text("29")),
+              DataCell(Text("Android Developer")),
+            ]),
+            DataRow(cells: [
+              DataCell(Text("Akter")),
+              DataCell(Text("29")),
+              DataCell(Text("Flutter Developer")),
+            ]),
+            DataRow(cells: [
+              DataCell(Text("Rashed")),
+              DataCell(Text("29")),
+              DataCell(Text("React Native Developer")),
+            ]),
+          ],
+        ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
