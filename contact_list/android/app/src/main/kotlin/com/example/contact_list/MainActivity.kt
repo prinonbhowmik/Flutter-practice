90f41c0e34dd6cd6f30fb390cd599a6e7c079ee1
prinonbhowmik <prinonarnob@gmail.com>
2022-08-14 19:24:19 +0600

    contact

---
+++ contact_list/android/app/src/main/kotlin/com/example/contact_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contact_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ contact_list/lib/main.dart
@@ -0,0 +1,86 @@
+import 'dart:typed_data';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Contact>? contacts;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getContact();
+  }
+
+  void getContact() async {
+    if (await FlutterContacts.requestPermission()) {
+      contacts = await FlutterContacts.getContacts(
+          withProperties: true, withPhoto: true);
+      print(contacts);
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "***  App Name  ***",
+            style: TextStyle(color: Colors.blue),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: (contacts) == null
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+          itemCount: contacts!.length,
+          itemBuilder: (BuildContext context, int index) {
+            Uint8List? image = contacts![index].photo;
+            String num = (contacts![index].phones.isNotEmpty) ? (contacts![index].phones.first.number) : "--";
+            return ListTile(
+                leading: (contacts![index].photo == null)
+                    ? const CircleAvatar(child: Icon(Icons.person))
+                    : CircleAvatar(backgroundImage: MemoryImage(image!)),
+                title: Text(
+                    "${contacts![index].name.first} ${contacts![index].name.last}"),
+                subtitle: Text(num),
+                onTap: () {
+                  if (contacts![index].phones.isNotEmpty) {
+                    launch('tel: ${num}');
+                  }
+                });
+          },
+        ));
+  }
+}
